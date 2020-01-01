e5261bd2be4cd56a87812371a903b456f603f1eb
Hero Intouch <Intouch.pr@ku.th>
2020-01-01 17:17:26 +0700

    Updates

---
+++ precache-manifest.3e892260b5eb2d2a4e98d08ef0acd915.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571df6a3ab7cf25b569e2d2faea43f3f",
+    "url": "/DrainLyzKU/index.html"
+  },
+  {
+    "revision": "f66e561aa29aee578629",
+    "url": "/DrainLyzKU/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d67fdbecfe56d1a2398e",
+    "url": "/DrainLyzKU/static/css/main.45fbbd46.chunk.css"
+  },
+  {
+    "revision": "f66e561aa29aee578629",
+    "url": "/DrainLyzKU/static/js/2.0cc7c765.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/DrainLyzKU/static/js/2.0cc7c765.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d67fdbecfe56d1a2398e",
+    "url": "/DrainLyzKU/static/js/main.63360b8a.chunk.js"
+  },
+  {
+    "revision": "8d494f6f6b35882bb56a",
+    "url": "/DrainLyzKU/static/js/runtime-main.2fbaa22c.js"
+  },
+  {
+    "revision": "a32945e2b0f3285c4eebfc1178dfca45",
+    "url": "/DrainLyzKU/static/media/Event1-1.a32945e2.jpg"
+  },
+  {
+    "revision": "9610ad7da7801e78070b3b213bde9117",
+    "url": "/DrainLyzKU/static/media/Event1-2.9610ad7d.jpg"
+  },
+  {
+    "revision": "d45d8b4c739b249d8a52872f97cd9d28",
+    "url": "/DrainLyzKU/static/media/Event2.d45d8b4c.JPG"
+  },
+  {
+    "revision": "9358435ce0d77421e8833157509cf60f",
+    "url": "/DrainLyzKU/static/media/Event3.9358435c.png"
+  },
+  {
+    "revision": "6be17c38b7bbd83f8ab3565b7521cf20",
+    "url": "/DrainLyzKU/static/media/Problem_pic.6be17c38.jpg"
+  }
+]);
